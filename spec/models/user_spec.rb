45ff0aa60177dac4a5b218f5881db98833cc56b7
Nikki Sharifli <nikkis@nulogy.com>
2017-05-29 16:34:54 -0400

    Creates spec for user model without factory girl

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "takes valid email addresses" do
+    user = User.create(email: 'example@google.com', password: 'password')
+
+    # validate user
+    result = user.valid?
+
+    # Expects to return an error message
+    expect(result).to eq(true)
+  end
 end
