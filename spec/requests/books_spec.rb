5fdc56f0661640ec0c6d82696800964902487e63
Nikki Sharifli <nikkis@nulogy.com>
2017-05-31 14:06:24 -0400

    Passes request specs for books index page

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 RSpec.describe BooksController, type: :request do
   describe "GET #index" do
     it "returns list of books" do
-      @book_1 = FactoryGirl.create(:book, ibn: "1", title: "Book1")
-      @book_2 = FactoryGirl.create(:book, ibn: "2", title: "Book2")
+      book_1 = FactoryGirl.create(:book, ibn: "1", title: "Book1")
+      book_2 = FactoryGirl.create(:book, ibn: "2", title: "Book2")
 
       get books_path
 
