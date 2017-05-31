74a696c154a883724f4b7ccb9f0537b321e8e5c3
Nikki Sharifli <nikkis@nulogy.com>
2017-05-31 16:34:41 -0400

    Creates request test for book show page

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 
 RSpec.describe BooksController, type: :request do
-  describe "GET #index" do
-    it "returns list of books" do
+    it "index page returns list of books" do
       book_1 = FactoryGirl.create(:book, ibn: "1", title: "Book1")
       book_2 = FactoryGirl.create(:book, ibn: "2", title: "Book2")
 
@@ -12,5 +11,12 @@ RSpec.describe BooksController, type: :request do
       expect(response.body).to include('Book1')
       expect(response.body).to include('Book2')
     end
-  end
+
+    it "show page returns details of book" do
+      book_1 = FactoryGirl.create(:book, ibn: "1", title: "Book1")
+
+      get book_path(book_1.id)
+
+      expect(response.body).to include('Book1')
+    end
 end
