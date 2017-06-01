1233848d73956bf2d973ccc6ba2484c28da8f7c6
Nikki Sharifli <nikkis@nulogy.com>
2017-06-01 16:32:46 -0400

    Creates failing feature spec for books that are not available for borrow

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,6 +4,7 @@ require_relative '../support/pages/Book_object'
 describe "User", type: :feature, driver: :selenium do
   it "checks out book through book details page" do
     book_1 = FactoryGirl.create(:book, ibn: "1", title: "Book1")
+    book_2 = FactoryGirl.create(:book, ibn: "2", title: "Book2")
     user_1 = FactoryGirl.create(:user, email: "foo@bar.com")
 
     visit user_session_path
@@ -15,5 +16,9 @@ describe "User", type: :feature, driver: :selenium do
     click_button("Borrow")
 
     expect(page).to have_selector(".notice", :text => "Successfully borrowed")
+
+    visit book_path(book_2.id)
+
+    expect(page).to have_no_selector(".borrow-button")
   end
 end
