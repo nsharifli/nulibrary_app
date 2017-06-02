4332839557360a54c2990903ce9258fe08140f50
Nikki Sharifli <nikkis@nulogy.com>
2017-06-02 12:18:34 -0400

    Creates borrow and current_quantity methods for inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,9 +1,14 @@
 class Inventory < ApplicationRecord
   belongs_to :book
 
-  def self.borrow(id)
-    inventory = Inventory.find_by(book_id: id)
+  def self.borrow(book_id)
+    inventory = Inventory.find_by(book_id: book_id)
     inventory.current_quantity -= 1
-    inventory.save
+    inventory.save!
+  end
+
+  def self.current_quantity(book_id)
+    inventory = Inventory.find_by(book_id: book_id)
+    inventory.present? ? inventory.current_quantity : 0
   end
 end
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  it "reduces inventory when book is borrowed" do
+  it "is reduced by one when a book is borrowed" do
     book_1 = FactoryGirl.create(:book, ibn: 1, title: "Book1")
     inventory_1 = FactoryGirl.create(:inventory, total_quantity: 4, current_quantity: 2, book: book_1)
 
     expect do
       Inventory.borrow(book_1.id)
     end.to change { inventory_1.reload.current_quantity }.by(-1)
+  end
+
+  describe "#current quantity" do
+    it "returns current quantity when the book is in inventory" do
+      book_1 = FactoryGirl.create(:book, ibn: 1, title: "Book1")
+      FactoryGirl.create(:inventory, total_quantity: 4, current_quantity: 2, book: book_1)
+
+      expect(Inventory.current_quantity(book_1.id)).to eq(2)
+    end
+
+    it "returns zero when the book is not in inventory" do
+      book_1 = FactoryGirl.create(:book, ibn: 1, title: "Book1")
+
+      expect(Inventory.current_quantity(book_1.id)).to eq(0)
+    end
   end
 end
