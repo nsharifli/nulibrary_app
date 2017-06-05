bfa0e63362e5d0ef177b013447f7f06cf8a13076
Nikki Sharifli <nikkis@nulogy.com>
2017-06-05 14:19:28 -0400

    Refactors book validation implementation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,11 @@
 class Book < ApplicationRecord
   # Validation
   validates :ibn, presence: true, uniqueness: true
-  validate :ibn_valid_length, unless: Proc.new {|book| book.ibn.nil?}
+  validate :ibn_valid_length
 
   def ibn_valid_length
-    if ibn.length != 10 && ibn.length != 13
+    length = ibn.try(:length)
+    if length != 10 && length != 13
       errors.add(:ibn, "Length should be either 10 or 13")
     end
   end
