67591bc43fd368e42cc79a8637d02bdddbb930f3
Nikki Sharifli <nikkis@nulogy.com>
2017-06-05 15:39:11 -0400

    Refactoring of inventory model specs

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
   it "is reduced by one when a book is borrowed" do
-    book_1 = FactoryGirl.create(:book, ibn: 1, title: "Book1")
+    book_1 = FactoryGirl.create(:book)
     inventory_1 = Inventory.find_by(book_id: book_1.id)
 
     expect do
@@ -12,13 +12,13 @@ RSpec.describe Inventory, type: :model do
 
   describe "#current quantity" do
     it "returns current quantity when the book is in inventory" do
-      book_1 = FactoryGirl.create(:book, ibn: 1, title: "Book1")
+      book_1 = FactoryGirl.create(:book)
 
       expect(Inventory.current_quantity(book_1.id)).to eq(1)
     end
 
     it "returns zero when the book is not in inventory" do
-      book_1 = FactoryGirl.create(:book, ibn: 1, title: "Book1")
+      book_1 = FactoryGirl.create(:book)
       Inventory.find_by(book_id: book_1.id).update_attributes(current_quantity: 0)
 
       expect(Inventory.current_quantity(book_1.id)).to eq(0)
