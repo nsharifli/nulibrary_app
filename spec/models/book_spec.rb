7e29ee754c4ad8c28d656f8025f6a538438a00f1
Nikki Sharifli <nikkis@nulogy.com>
2017-06-05 11:14:48 -0400

    Creates test for book(ibn) validation

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-
+  it "is not valid if ibn is nil" do
+    book_1 = FactoryGirl.create(:book, ibn: nil, title: "Book1")
+    validation = book_1.valid?
+    expect(validation).to eq(false)
+  end
 end
