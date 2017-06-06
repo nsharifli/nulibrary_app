b913c5d543c0f9de31323a63f557fdfa43609bc9
Nikki Sharifli <nikkis@nulogy.com>
2017-06-06 14:56:48 -0400

    Changes seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 FactoryGirl.create(:user, email: 'test@test.com', password: 'password')
-book_1 = FactoryGirl.create(:book, title: 'New Book', ibn: '12345')
-FactoryGirl.create(:inventory, total_quantity: 10, current_quantity: 4, book: book_1)
+FactoryGirl.create(:book, title: 'Crime and Punishment')
+FactoryGirl.create(:book, title: 'Foundation')
