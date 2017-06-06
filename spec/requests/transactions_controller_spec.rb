647d338a9696b6272f1180713bd6b347802a055c
Nikki Sharifli <nikkis@nulogy.com>
2017-06-06 17:56:00 -0400

    Creates request spec for transaction#index

---
+++ spec/requests/transactions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe TransactionsController, type: :request do
+  describe "GET transactionss#index" do
+    it "index page returns list of books user checked out" do
+      book_1 = FactoryGirl.create(:book, title: "Book1")
+      FactoryGirl.create(:book, title: "Book2")
+      user = FactoryGirl.create(:user)
+
+      sign_in user
+
+
+      get transactions_path
+
+      expect(response.body).to include(book_1.title)
+      expect(response.body).to include("Return")
+    end
+  end
+end
