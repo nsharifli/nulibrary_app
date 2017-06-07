597a01575e4181b64e7956cad5c6450aac6153b8
Nikki Sharifli <nikkis@nulogy.com>
2017-06-07 12:14:32 -0400

    Creates request spec for transactions when user is not signed in

--- spec/requests/transactions_controller_spec.rb
+++ spec/requests/transactions_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe TransactionsController, type: :request do
   describe "GET transactionss#index" do
-    it "index page returns list of books user checked out" do
+    it "index page returns list of books user checked out if user is signed in" do
       book_1 = FactoryGirl.create(:book, title: "Book1")
       user = FactoryGirl.create(:user)
       FactoryGirl.create(:transaction, :unreturned, book: book_1, user: user)
@@ -14,5 +14,13 @@ RSpec.describe TransactionsController, type: :request do
       expect(response.body).to include(book_1.title)
       expect(response.body).to include("Return")
     end
+
+    it "index page responds with 403 if user is not signed in" do
+      get transactions_path
+
+      expect(response.status).to eq(403)
+    end
   end
+
+
 end
