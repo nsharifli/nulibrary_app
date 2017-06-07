877d55138daae3a05ebe210bcff8e754fd6809e9
Nikki Sharifli <nikkis@nulogy.com>
2017-06-06 22:32:00 -0400

    Adds add_borrow_entry method to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  def self.add_borrow_entry(user, book)
+    Transaction.create(user: user, book: book, borrowed_at: DateTime.now)
+  end
 end
