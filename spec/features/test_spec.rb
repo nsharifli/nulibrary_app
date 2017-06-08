b5dadf2d4d130b545e7132d7a3b962df28c7eafd
Nikki Sharifli <nikkis@nulogy.com>
2017-06-08 14:22:16 -0400

    Deletes google.com test feature

--- spec/features/test_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Google", type: :feature do
-  it "visits google.com" do
-    visit 'https://www.google.com'
-    expect(page).to have_content 'Google'
-  end
-
-end
