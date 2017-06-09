b9ffec5783a8c1d7fa5c5fdc0777f6430aa85191
Nikki Sharifli <nikkis@nulogy.com>
2017-06-09 18:02:32 -0400

    Passes specs for find_description method in google books adapter

--- app/services/google_books_adapter.rb
+++ app/services/google_books_adapter.rb
@@ -2,4 +2,8 @@ module GoogleBooksAdapter
   def self.find_title(isbn)
     GoogleBooks.search("isbn:#{isbn}").first.title
   end
+
+  def self.find_description(isbn)
+    GoogleBooks.search("isbn:#{isbn}").first.description
+  end
 end
