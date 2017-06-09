4b19e062dd340ae486d1e7aafbcebaae967c92ee
Nikki Sharifli <nikkis@nulogy.com>
2017-06-09 18:05:43 -0400

    Passes spec for google books adapter find_author method

--- app/services/google_books_adapter.rb
+++ app/services/google_books_adapter.rb
@@ -6,4 +6,8 @@ module GoogleBooksAdapter
   def self.find_description(isbn)
     GoogleBooks.search("isbn:#{isbn}").first.description
   end
+
+  def self.find_author(isbn)
+    GoogleBooks.search("isbn:#{isbn}").first.authors
+  end
 end
