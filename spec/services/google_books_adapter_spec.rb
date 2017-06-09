a6810e76cd1f5ef91486db6f1650da499b41416f
Nikki Sharifli <nikkis@nulogy.com>
2017-06-09 17:59:27 -0400

    Creates Google books adapter unit spec for find_description method

--- spec/services/google_books_adapter_spec.rb
+++ spec/services/google_books_adapter_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe GoogleBooksAdapter, type: :unit do
+  let(:isbn) {"0321721330"}
+  let(:google_books) {double("Google Books")}
+
   it "finds title of a book given an isbn number" do
-    isbn = "0321721330"
-    google_books = double("Google Books")
     allow(google_books).to receive_message_chain(:first, :title).and_return("Book title")
 
     allow(GoogleBooks).to receive(:search).with("isbn:#{isbn}").and_return(google_books)
@@ -11,5 +12,15 @@ RSpec.describe GoogleBooksAdapter, type: :unit do
     result = GoogleBooksAdapter.find_title(isbn)
 
     expect(result).to eq("Book title")
+  end
+
+  it "finds description of a book given an isbn number" do
+    allow(google_books).to receive_message_chain(:first, :description).and_return("Book description")
+
+    allow(GoogleBooks).to receive(:search).with("isbn:#{isbn}").and_return(google_books)
+
+    result = GoogleBooksAdapter.find_description(isbn)
+
+    expect(result).to eq("Book description")
   end
 end
