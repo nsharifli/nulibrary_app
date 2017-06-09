63d6277777ce725bd637eec0c86866656aeabf13
Nikki Sharifli <nikkis@nulogy.com>
2017-06-09 17:48:01 -0400

    Creates Google books adapter unit spec

--- app/services/google_books_adapter.rb
+++ app/services/google_books_adapter.rb
@@ -1,5 +1,5 @@
 module GoogleBooksAdapter
   def self.find_title(isbn)
-
+    GoogleBooks.search("isbn:#{isbn}").first.title
   end
 end
--- spec/services/google_books_adapter_spec.rb
+++ spec/services/google_books_adapter_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe GoogleBooksAdapter, type: :unit do
   it "finds title of a book given an isbn number" do
     isbn = "0321721330"
+    google_books = double("Google Books")
+    allow(google_books).to receive_message_chain(:first, :title).and_return("Book title")
 
-    allow(GoogleBooks).to receive(:search).with("isbn:#{isbn}").and_return("Book title")
+    allow(GoogleBooks).to receive(:search).with("isbn:#{isbn}").and_return(google_books)
 
     result = GoogleBooksAdapter.find_title(isbn)
 
