ad83690e61d99d05b976154bd511e5a25e516b51
Nikki Sharifli <nikkis@nulogy.com>
2017-06-10 17:55:12 -0400

    Passes unit spec for Google books adapter where books is not found

--- app/services/google_books_adapter.rb
+++ app/services/google_books_adapter.rb
@@ -1,23 +1,22 @@
 module GoogleBooksAdapter
   def self.find_title(isbn)
-    book(isbn).title
+    book(isbn).nil? ? nil : book(isbn).title
   end
 
   def self.find_description(isbn)
-    book(isbn).description
+    book(isbn).nil? ? nil : book(isbn).description
   end
 
   def self.find_author(isbn)
-    book(isbn).authors
+    book(isbn).nil? ? nil : book(isbn).authors
   end
 
   def self.find_image_link(isbn)
-    book(isbn).image_link
+    book(isbn).nil? ? nil : book(isbn).image_link
   end
 
   private
   def self.book(isbn)
     GoogleBooks.search("isbn:#{isbn}").first
   end
-
 end
