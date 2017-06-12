21368a072e85ef7ad25237b3b7f2fd927a764fa7
Nikki Sharifli <nikkis@nulogy.com>
2017-06-12 13:42:36 -0400

    Creates spec for BookFactory#create method when quantity is negative

--- spec/services/book_factory_spec.rb
+++ spec/services/book_factory_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe BookFactory, type: :unit do
   describe "#create" do
-    it "returns false is book is not found" do
+    it "returns false if book is not found" do
       isbn = "1234567890"
       allow(GoogleBooksAdapter).to receive(:find_title).with(isbn).and_return(nil)
 
       result = BookFactory.create(isbn: isbn, quantity: 2)
+
+      expect(result).to eq(false)
+    end
+
+    it "returns false if quantity is less than zero" do
+      isbn = "1234567890"
+      quantity = -2
+      allow(GoogleBooksAdapter).to receive(:find_title).with(isbn).and_return("Book title")
+
+      result = BookFactory.create(isbn: isbn, quantity: quantity)
 
       expect(result).to eq(false)
     end
