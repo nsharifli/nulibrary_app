be0774194083fe4aa26cc5d0c17b4e34c6398a65
Nikki Sharifli <nikkis@nulogy.com>
2017-06-16 13:25:52 -0400

    Passes unit specs for BookReturnService#borrow method when return is not successful

--- spec/services/book_return_service_spec.rb
+++ spec/services/book_return_service_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe BookReturnService, type: :unit do
 
       expect(result).to eq(true)
     end
+
+    it "doesn't return book successfully if book is already returned" do
+      allow(BookReturnService).to receive(:increase_inventory).with(book).and_raise("ValidationError")
+      allow(BookReturnService).to receive(:update_book_transaction).with(user, book.id).and_return(true)
+
+      expect { BookReturnService.return(book: book, user: user) }.to raise_error("ValidationError")
+    end
+
+    it "doesn't return book successfully if book is not borrowed by user" do
+      allow(BookReturnService).to receive(:increase_inventory).with(book).and_return(true)
+      allow(BookReturnService).to receive(:update_book_transaction).with(user, book.id).and_raise("ValidationError")
+
+      expect { BookReturnService.return(book: book, user: user) }.to raise_error("ValidationError")
+    end
   end
 
 end
