394c752475182e64c0d5f0e5cb28bfa6a1efb9f8
Nikki Sharifli <nikkis@nulogy.com>
2017-06-19 18:03:30 -0400

    Implements pagination feature for transactions index page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,6 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @my_transactions = Transaction.where(user: current_user)
+    @my_transactions = Transaction.where(user: current_user, returned_at: nil).order(borrowed_at: :desc).paginate(page: params[:page], per_page: 5)
   end
 end
