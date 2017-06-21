656d2885cad9dc2f1bd8cce51b1dc4111702fab9
Nikki Sharifli <nikkis@nulogy.com>
2017-06-21 13:19:56 -0400

    Creates model spec for transaction model #currently_reading_user method

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
+  describe "#currently_reading_users" do
+    it "returns the user who currently has that book" do
+      book_1 = FactoryGirl.create(:book)
+      user_1 = FactoryGirl.create(:user)
+      FactoryGirl.create(:transaction, :unreturned, book: book_1, user: user_1)
+
+      result = Transaction.currently_reading_user(book: book_1)
+
+      expect(result).to eq(user_1.id)
+    end
+  end
 
 end
 
