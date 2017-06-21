b0dbe9c4070b341cd7b434d35d8c47951c4ca8f4
Nikki Sharifli <nikkis@nulogy.com>
2017-06-21 13:20:17 -0400

    Passes model spec for transaction model #currently_reading_user method

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  def self.currently_reading_user(book:)
+    transaction = Transaction.find_by(book: book, returned_at: nil)
+    transaction.user_id
+  end
 end
