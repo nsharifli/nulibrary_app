32409c897e94c489d5665e4ab09deb6e8a7bdf8e
Nikki Sharifli <nikkis@nulogy.com>
2017-06-27 17:40:42 -0400

    Creates model specs for Hold#open_hold_request_exists? method

--- spec/models/hold_spec.rb
+++ spec/models/hold_spec.rb
@@ -1,4 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Hold, type: :model do
+
+  describe "#open_hold_request_exists?" do
+    let(:user_1) { FactoryGirl.create(:user) }
+    let(:book_1) { FactoryGirl.create(:book) }
+
+    it "returns true if request is not closed yet" do
+      FactoryGirl.create(:hold, user: user_1, book: book_1)
+
+      result = Hold.open_hold_request_exists?(book: book_1, user: user_1)
+
+      expect(result).to eq(true)
+    end
+
+    it "returns false if request is closed" do
+      FactoryGirl.create(:hold, user: user_1, book: book_1, closed_at: Time.zone.now)
+
+      result = Hold.open_hold_request_exists?(book: book_1, user: user_1)
+
+      expect(result).to eq(false)
+    end
+
+    it "returns false if there is not a request" do
+      result = Hold.open_hold_request_exists?(book: book_1, user: user_1)
+
+      expect(result).to eq(false)
+    end
+  end
 end
